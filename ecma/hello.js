e4bb6455ffb626698cd98b985d2cd860a07bb073
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-11-26 12:28:48 +0100

    add hello world

---
+++ ecma/hello.js
@@ -0,0 +1,11 @@
+/*
+Die Hello World als erste Anwendung
+*/
+
+//Deklaration, Zuweisung, Literal
+let message = "Hello World!" 
+
+//Was ist mit var? Wird unterstützt, sollte aber nicht mehr benutzt werden
+
+//Konsolen-Ausgabe, OOP durch den Punkt-Operator
+console.log(message)
