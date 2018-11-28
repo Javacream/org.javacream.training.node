07781ad21df5de20001b9be400464d5861354107
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-11-28 11:27:09 +0100

    add add promise example with thirdSyncFunction

--- ecma/promises.js
+++ ecma/promises.js
@@ -12,8 +12,14 @@ function firstAsyncFunction(delay){
       });
       return result;
   }
+  function thirdSyncFunction(param){
+    console.log("Entering thirdFunction at " + new Date())
+    return `Third Function returns ${param}`;
+}
 
-  console.log("calling firstAsyncFunction at " + new Date())
+console.log("calling firstAsyncFunction at " + new Date())
 console.log("finished...")
 firstAsyncFunction(1000).then((res) => secondAsyncFunction(res, 2000)).then((res) => console.log(`Resolving result ${res} at ${new Date()}`))
+firstAsyncFunction(1000).then((res) => secondAsyncFunction(res, 2000)).then((res) => console.log(thirdSyncFunction(`Resolving result ${res} at ${new Date()}`)))
 Promise.all([firstAsyncFunction(1000), secondAsyncFunction("all", 1000)]).then((res) => {console.log("Finishing Promise.all at " + new Date() + res)})
+Promise.all([firstAsyncFunction(1000), secondAsyncFunction("all", 1000), thirdSyncFunction("Hugo")]).then((res) => {console.log("Finishing Promise.all at " + new Date() + res)})
