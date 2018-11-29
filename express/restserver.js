1e88e1c13e839f0d3b5618729addf000c301d7e8
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-11-29 09:34:10 +0100

    add simple rest server curl scripts

--- express/restserver.js
+++ express/restserver.js
@@ -10,7 +10,16 @@ app.get("/service/ping", (req, res) => {
     res.status(200).send("pong");
   });
 
-
+app.get("/service/greeter/:name", (req, res) => {
+    let name = req.params.name
+    res.status(200).send(`Hello ${name}!`);
+  });
+app.post("/service/greeter", (req, res) => {
+    let person = req.body
+    console.log(person)
+    console.log(`Person: name=${person.name}, height=${person.height}`)
+    res.status(200).send(`Hello ${person.name}!`);
+  });
 let server = app.listen(3000, () => {
-    console.log("server running on port:", server.address().port);
+    console.log(`server running on port: ${server.address().port}`);
 });  
