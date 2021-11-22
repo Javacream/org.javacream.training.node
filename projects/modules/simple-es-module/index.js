78162faeea25960f8c7eb1b19688da55c9f0f7ee
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-11-22 12:06:54 +0100

    introduce es module

---
+++ projects/modules/simple-es-module/index.js
@@ -0,0 +1,2 @@
+export function circlePerimeter (radius) {return 2 * Math.PI * radius}
+export function circleArea(radius) {return Math.PI * radius * radius}
--- projects/simple-app/index.js
+++ projects/simple-app/index.js
@@ -1,2 +1,5 @@
-let circleUtil = require('simple-commonsjs-module')
+//let circleUtil = require('simple-commonsjs-module')
+import circleUtil from 'simple-commonsjs-module'
+import {circleArea} from 'simple-es-module'
 console.log(circleUtil.circleArea(1))
+console.log(circleArea(1))
